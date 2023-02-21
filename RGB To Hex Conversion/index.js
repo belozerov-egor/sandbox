6cc55a23db1ea76e939cf6930858a182401073ba
Belozerov Egor <belaz.egor@gmail.com>
2023-02-21 16:02:47 +0300

    new task

---
+++ RGB To Hex Conversion/index.js
@@ -0,0 +1,14 @@
+function rgb(r, g, b){
+  // complete this function  
+ return toHex(r)+toHex(g)+toHex(b);
+}
+function toHex(d) {
+    if(d < 0 ) {return "00";}
+    if(d > 255 ) {return "FF";}
+    return  ("0"+(Number(d).toString(16))).slice(-2).toUpperCase()
+}
+function rgb(r, g, b){
+  return [r,g,b].map(function(x) {
+    return ('0'+Math.max(0, Math.min(255, x)).toString(16)).slice(-2);
+  }).join('').toUpperCase();
+  }
